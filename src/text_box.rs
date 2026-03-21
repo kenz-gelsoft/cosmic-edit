6d9c9123da91ec338a7996df9d9bf90f08ab2586
KENZ <KENZ.gelsoft@gmail.com>
2026-03-21 12:09:40 +0900

    Fix the input method window occasionally jumps around
    Use the previous caret(text cursor) position to place the input method
    window when the position is not available

--- src/text_box.rs
+++ src/text_box.rs
@@ -133,7 +133,6 @@ where
     fn input_method<'b>(
         &self,
         state: &'b State,
-        editor: &BorrowedWithFontSystem<'_, ViEditor<'static, 'static>>,
         scale_factor: f32,
         layout: Layout<'_>,
     ) -> InputMethod<&'b str> {
@@ -143,7 +142,7 @@ where
         };
 
         let editor_pos = layout.position() + [self.padding.left, self.padding.top].into();
-        let (caret_x, caret_y) = editor.cursor_position().unwrap_or_default();
+        let (caret_x, caret_y) = state.caret_position;
         InputMethod::Enabled {
             cursor: Rectangle::new(
                 Point::new(
@@ -1398,12 +1397,8 @@ where
             }
             Event::Window(window::Event::RedrawRequested(_now)) => {
                 if state.is_focused {
-                    shell.request_input_method(&self.input_method(
-                        state,
-                        &editor,
-                        scale_factor,
-                        layout,
-                    ));
+                    state.caret_position = editor.cursor_position().unwrap_or(state.caret_position);
+                    shell.request_input_method(&self.input_method(state, scale_factor, layout));
                 }
             }
             _ => (),
@@ -1455,6 +1450,7 @@ pub struct State {
     scrollbar_h_rect: Cell<Option<Rectangle<f32>>>,
     handle_opt: Mutex<Option<image::Handle>>,
     shift_anchor: Mutex<Option<Cursor>>,
+    caret_position: (i32, i32),
     preedit: Option<input_method::Preedit>,
 }
 
@@ -1473,6 +1469,7 @@ impl State {
             scrollbar_h_rect: Cell::new(None),
             handle_opt: Mutex::new(None),
             shift_anchor: Mutex::new(None),
+            caret_position: (0, 0),
             preedit: None,
         }
     }
